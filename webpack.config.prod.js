d7570a1c6df6a3fd45035fd071682b5f50a4cb05
Viktar Svintsitski <jetalai3@gmail.com>
2021-01-27 00:39:48 +0300

    feat: add separate production config for webpack

---
+++ webpack.config.prod.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    port: 9000,
+    contentBase: path.join(__dirname, "src"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Home Player",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+};
